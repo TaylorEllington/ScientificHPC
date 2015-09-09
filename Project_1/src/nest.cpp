3d1e287188df1d92ff85d4cde8823b2d77c63c09
Taylor Ellington <taylorellington101@gmail.com>
2015-09-09 14:13:44 -0500

    finished proj1.cpp and started the ipynb

--- Project_1/src/nest.cpp
+++ Project_1/src/nest.cpp
@@ -2,8 +2,12 @@
 #include "nest.hpp"
 
 	double nest ( const Matrix& a, double x ){
-		int size = a.Size();
-		double p;
+		int size = a.Size() -1 ;
+		double p = a(size);
 
-		return size;
+		for( int i = size -1;  i >= 0 ; i--){
+			p = a(i) + ( x*p ) ;
+		}
+
+		return p;
 	}
--- Project_1/src/proj1_a.cpp
+++ Project_1/src/proj1_a.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <math.h>
 using namespace std;
 
 #include <iostream>
@@ -12,11 +13,98 @@ using namespace std;
 
 int main(int argc, char * argv[]){
 
-    Matrix mat("1, 2 , 3 , 4");
-    
+  
 
-	double derp = nest( mat , 4.0 );
-	cout<< derp;
+
+
+	//create row vector z, from -3 to 3
+    Matrix z =Linspace(-3, 3, 1, 600);
+
+    //create vector of coeficents for taylor polynimial for cos x
+
+    double coeff_temp[13] = {
+    	1,
+		0,
+ 		-0.5,
+  		0,
+   		0.04166666666666666666666666,
+   		0,
+   		-0.00138888888888888888888888,
+    	0, 
+		0.0000248015873015873015873015873015873015873015873015,
+ 		0,
+  		-0.000000027557319223985890652,
+   		0,
+    	0.0000000020876756987868098979210090321201432312543423654534765645};
+
+    double coeff_temp2[6] = {
+		
+ 		-0.5,
+  		
+   		0.04166666666666666666666666,
+   		
+   		-0.00138888888888888888888888,
+    	
+		0.0000248015873015873015873015873015873015873015873015,
+ 		
+  		-0.000000027557319223985890652,
+   		
+    	0.0000000020876756987868098979210090321201432312543423654534765645};
+  
+    //compute p4
+    Matrix coeff4(1, 4, coeff_temp);
+    Matrix  p4(z.Size());
+    for(int i = 0; i < 600; i++){
+    	p4(i) =  nest(coeff4, z(i));
+    }
+   
+    //compute p8
+    Matrix coeff8(1, 8, coeff_temp);
+    Matrix  p8(z.Size());
+    for(int i = 0; i < 600; i++){
+    	p8(i) = nest(coeff8, z(i));
+    }
+  
+    //compute p12
+    Matrix coeff12(1, 12, coeff_temp);
+    Matrix  p12(z.Size());
+    for(int i = 0; i < 600; i++){
+    	p12(i) =  nest(coeff12, z(i));
+    }
+    
+    //compute f
+    Matrix  f(z.Size());
+    for(int i = 0; i < 600; i++){
+    	f(i) = cos( z(i) );
+    }
+
+    //compute err4
+    Matrix  err4(z.Size());
+    for(int i = 0; i < 600; i++){
+    	err4(i) = abs(f(i) - p4(i));
+    }
+    //compute err8
+    Matrix  err8(z.Size());
+    for(int i = 0; i < 600; i++){
+    	err8(i) = abs(f(i) - p8(i));
+    }
+    //compute err12
+    Matrix  err12(z.Size());
+    for(int i = 0; i < 600; i++){
+    	err12(i) = abs(f(i) - p12(i));
+    }
+    z.Write("z.txt");
+    p4.Write("p4.txt");
+    p8.Write("p8.txt");
+    p12.Write("p12.txt");
+    f.Write("f.txt");
+    err4.Write("err4.txt");
+    err8.Write("err8.txt");
+    err12.Write("err12.txt");
+
+
+
+ 
 
 	return 0;
 }
