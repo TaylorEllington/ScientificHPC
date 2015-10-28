090757d4a7bdbf4a982f90c6991503091af98181
Taylor Ellington <taylorellington101@gmail.com>
2015-10-28 14:49:19 -0500

    proj 3 scaffolding

---
+++ Project_3/src/Lagrange2D.cpp
@@ -0,0 +1,4 @@
+double Lagrange2D(Matrix& x, Matrix& y, Matrix& f, double a, double b){
+
+	return 0.0;
+}
---
+++ Project_3/src/Newton_interpolant.cpp
@@ -0,0 +1,14 @@
+#include "matrix.cpp"
+
+
+Matrix Newton_coefficients(Matrix& x, Matrix& y){
+	Matrix a();
+
+	return a; 
+}
+
+double Newton_evaluate(Matrix& x, Matrix& c, double z){
+
+
+	return 0.0
+}
