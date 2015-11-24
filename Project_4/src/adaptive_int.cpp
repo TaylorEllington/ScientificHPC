19cb12f44de01c4aa648bcc74a88fb5501941c9b
Taylor Ellington <taylorellington101@gmail.com>
2015-11-24 10:46:28 -0600

    Project 4 checkin, parts 1 and 2. 2 not optimal, but it works

--- Project_4/src/adaptive_int.cpp
+++ Project_4/src/adaptive_int.cpp
@@ -3,18 +3,58 @@
 *   adaptave_int.cpp
 *   11/10/2015
 */
+#include <math.h>
 #include "fcn.hpp"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+double composite_int(Fcn& f, const double a, 
+			const double b, const int n);
 
 int adaptive_int(Fcn& f, const double a, const double b, const double rtol,
                      const double atol, double& R, int& n, int& Ntot){
-	int result = 0;
+	
 
 
+	int n_temp, Ntot_temp, problem_condition;
+
+	double R_approx, I_approx, error, bound;
+
+	bool working = true; 
+	n_temp = 5;
+	Ntot_temp = 0;
+
+	while( working == true){
+
+		R_approx = composite_int(f,  a,  b,  n_temp);
+		I_approx = composite_int(f,  a,  b,  n_temp+100);
 
 
+		error = fabs(I_approx - R_approx);
+		bound = (rtol*fabs(I_approx)) + atol;
 
+		
+		Ntot_temp += n_temp;
+		if(n_temp > 330){
+			working = false;
+			problem_condition = 1;
 
+		} else if((error) < (bound)){
+			working = false;
+			problem_condition = 0;
 
-	return result;
+		} else {
+			n_temp += 5;
+		}
+
+	}
+
+	n =  n_temp;
+	Ntot = Ntot_temp;
+   	R = R_approx;
+
+	return problem_condition;
 
 }
--- Project_4/src/test_adapt.cpp
+++ Project_4/src/test_adapt.cpp
@@ -13,15 +13,14 @@
 
 using namespace std;
 
-double composite_int(Fcn& f, const double a, 
-			const double b, const int n);
+
 
 int adaptive_int(Fcn& f, const double a, const double b, const double rtol,
                      const double atol, double& R, int& n, int& Ntot);
 
 
 
-
+   
 class fcn : public Fcn {
 public:
   double c, d;
@@ -40,24 +39,36 @@ int main(int argc, char** argv){
   // limits of integration
   double a = -3.0;
   double b = 5.0;
+  
 
   // integrand
   fcn f;
   f.c = 0.5;
   f.d = 25.0;
 
+  double Itrue = f.antiderivative(b) - f.antiderivative(a);
+  printf("\n True Integral = %22.16e\n", Itrue);
+
   vector<double> r_tolerances = {1e-2, 1e-4, 1e-6,1e-8, 1e-10, 1e-12};
+  vector<double> a_tolerances = {1e-2/1000, 1e-4/1000, 1e-6/1000,1e-8/1000, 1e-10/1000, 1e-12/1000};
+  double R, relError, errorBound;
+  int n, Ntot, success;
 
-  double R;
-  int n, ntot;
+  cout << "\n Adaptive approximation:\n";
+  cout << "     R(f)                 |I(f) - R(f)|    rtol|I(f)| + atol         n     Ntot\n";
+  cout << "  ------------------------------------------------------------------------------\n";
+
+
+
   for (int i = 0 ; i < r_tolerances.size(); i++ ){
-  	cout << "\n Adaptive approximation:\n";
-  	cout << "     R(f)             |I(f) - R(f)|            rtol|I(f) + atol            n    Ntot\n";
-  	cout << "  ----------------------------------------------------------------------------------\n";
-
+  	
+    success = adaptive_int(f, a, b, r_tolerances[i], a_tolerances[i], R,  n, Ntot);
+    relError = fabs(Itrue - R);
+    errorBound = (r_tolerances[i]*Itrue) + a_tolerances[i];
+     printf("  %22.16e  %7.1e          %7.1e               %6i  %6i\n", R, relError, errorBound, n, Ntot);
   }
 
 
-
+  cout << "  ------------------------------------------------------------------------------\n";
 	return 0;
 }
